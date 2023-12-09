6c795ed590a369985c7610477b60293c0ec73e7e
tinghsuansu <tinghsuansu@users.noreply.github.com>
2023-12-08 20:17:50 -0500

    20231208 - FP

---
+++ final-project/js/script.js
@@ -0,0 +1,9 @@
+
+//use jQuery to load the same header and footer
+
+$(function(){
+    $("#linked-header").load("header.html"); 
+    $("#linked-header-project").load("header-project.html"); 
+    $("#linked-footer").load("footer.html"); 
+});
+
